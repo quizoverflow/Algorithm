7716372ca41c79b7c68d030e5a5ec58595867d30
jinu <jinu010219@naver.com>
2023-05-14 12:58:26 +0900

    git push test

--- Cpp/just_memo/one.cpp
+++ Cpp/just_memo/one.cpp
@@ -13,7 +13,33 @@ int main(){
     cout<<"[DEBUG] debug enabled!\n";
 #endif
     TIMME
-    cout<< "hello test";
-
+    int n, a, b;
+    cin >> n;
+    vector<int> tree[100001];
+    vector<int> parents(100001);
+    for(int i = 1; i < n; ++i){
+        cin >> a >> b;
+        tree[a].push_back(b);
+        tree[b].push_back(a);
+    }
+    queue<int> qu;
+    vector<bool> visited(100001,false);
+    visited[1] = true;
+    qu.push(1);
+    while(!qu.empty()){
+        int now = qu.front();
+        visited[now] = true;
+        qu.pop();
+        for(auto &a : tree[now]){
+            if(!visited[a]){
+                parents[a] = now;
+                qu.push(a);
+            }
+        }
+    }
+    for(int i = 2; i <= n; ++i){
+        cout<< parents[i] << "\n";
+    }
+    
     return 0;
 }
--- 채점 파일/generator/gen.bash
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-for i in {0..9}
-do
-  echo "Generating ${i}th test case..."
-  echo -e "200000\n$i" | python gen.py > $i.in
-  ./main < $i.in > $i.out
-done
-echo "Done!"
