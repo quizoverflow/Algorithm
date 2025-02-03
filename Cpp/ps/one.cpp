a290c0a2f6b3ce233763b0895e27fcb421bc95b3
jinuchoi <jinu010219@naver.com>
2025-02-03 14:29:39 +0900

    1002터렛

--- Cpp/ps/one.cpp
+++ Cpp/ps/one.cpp
@@ -1,6 +1,6 @@
 /*
-#BOJ 2110 공유기설치
-#이분탐색
+#BOJ 1002 터렛
+#기하학 #많은 조건분기
 */
 
 #include <bits/stdc++.h>
@@ -12,42 +12,81 @@
 #endif 
 using namespace std;
 
-int main(){
-    FASTIO
-    int n,c;
-    cin >> n >> c;
-    vector<int> house(n);
-    for(int i= 0; i < n; ++i){
-        cin >> house[i];
-    }
-    sort(house.begin(),house.end());
+#define power(x) (x) * (x)
 
-    int s = 1;
-    int e = house.back() - house.front();
-    int mid;
-    int ans = 0;
-    while(s <= e){
-        //mid == distance 
-        mid = (s + e) / 2;
-        
-        //check
-        int cnt = 1;
-        int prev = house.front();
-        for(int i = 1; i < n; ++i){
-            if(house[i] - prev >= mid){
-                prev = house[i];
-                cnt++;
-            }
-        }
-        if(cnt >= c){
-            ans = mid;
-            s = mid+1;
+struct Circle{
+    double x,y,r;
+};
+
+Circle c1,c2;
+double dsq;
+
+//case 1
+int outer_point(){
+    double ssq = power(c1.r + c2.r);
+    if(ssq > dsq){
+        return 2;
+    }
+    else if (ssq == dsq){
+        return 1;
+    }
+    else{
+        return 0;
+    }
+}
+
+//case 2
+int inner_point(){
+    double msq = power(c2.r - c1.r);
+    if(dsq < msq){
+        return 0;
+    }
+    else if(dsq == msq){
+        return 1;
+    }
+    else{
+        return 2;
+    }
+}
+
+//case 3
+int same_point(){
+    if(c1.r == c2.r) return -1;
+    else return 0;
+}
+
+void run_case(){
+    cin >> c1.x >> c1.y >> c1.r >> c2.x >> c2.y >> c2.r;
+    if(c2.r < c1.r){
+        swap(c1,c2);
+    }
+    dsq = power(c1.x - c2.x) + power(c1.y - c2.y);
+
+    int ans;
+    if(c1.x == c2.x && c1.y == c2.y){
+        ans = same_point();
+    }
+    else{
+        if(dsq <= c2.r * c2.r){
+            ans = inner_point();
         }
         else{
-            e = mid - 1;
+            ans = outer_point();
         }
     }
-    cout << ans;
+
+    cout << ans << "\n";
+}
+
+int main(){
+    FASTIO
+
+    int testcase;
+    cin >> testcase;
+
+    while(testcase--){
+        run_case();
+    }
 
     return 0;
 }
