d55e705fe8a3cb932a9411df47288f932ca2824d
jinuchoi <jinu010219@naver.com>
2025-02-09 19:52:59 +0900

    14442벽부수고 이동하기

--- Cpp/ps/test.cpp
+++ Cpp/ps/test.cpp
@@ -10,9 +10,19 @@ using namespace std;
 int main(){
     FASTIO
 
-    int num = -130;
+    vector<int> v = {1,2,3,4,5};
+    debug(v);
 
-    cout << num % 100 << endl;
+    vector<int> a = {9,9,9,9};
+
+    v.assign(a.begin(),a.end());
+    
+    cout << "#2"<<v.size() << endl;
+    debug(v);
+
+    v.assign(1,3);
+    cout <<"#3"<<v.size() << endl;
+    debug(v);
 
     return 0;
 }
