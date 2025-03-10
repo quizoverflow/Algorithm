8910308203faebef9e3f33968d52495bf015402a
jinuchoi <jinu010219@naver.com>
2025-03-10 10:49:11 +0900

    1011

--- Cpp/ps/test.cpp
+++ Cpp/ps/test.cpp
@@ -1,40 +1,18 @@
-#include <bits/stdc++.h>
-#define FASTIO ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
-#ifdef PSJINU
-#include "debug.h"
-#else
-#define debug(x...)
-#endif 
-using namespace std;
-
-int main(){
-    FASTIO
-
-
-    priority_queue<pair<int,int>,vector<pair<int,int>>,greater<>> pq;
-
-    vector<pair<int,int>> v ={
-        {1,0},
-        {1,1},
-        {9,2},
-        {8,4},
-        {10,1},
-        {9,3},
-        {1,-1}
-    };
-
-    for(int i = 0; i < v.size(); ++i){
-        pq.push(v[i]);
+#include<stdio.h>
+long t,x,y,n,d;
+main(){
+    scanf("%d",&t);
+    while(t--){
+        scanf("%ld %ld",&x,&y);
+        y-=x;
+        n=1;
+        while(1){
+            d=n*n+n;
+            if(d>=y)break;
+            ++n;
+        }
+        n*=2;
+        if(d-n/2>=y)--n;
+        printf("%d",n);
     }
-
-    while(!pq.empty()){
-       // cout << pq.top().first << " " << pq.top().second << endl;
-        pq.pop();
-    }
-
-    int i = 0;
-
-    cout << i * (i-1) / 2;
-    
-    return 0;
 }
